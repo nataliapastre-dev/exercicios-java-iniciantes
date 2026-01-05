500fc5cb5dd2c836e955257acf3550750bf455e7
Natália Baptista Pastre <natalia.pastre10@gmail.com>
2026-01-04 21:07:50 -0300

    Update ExemplosScanner.java

--- 03-entrada-de-dados/ExemplosScanner.java
+++ 03-entrada-de-dados/ExemplosScanner.java
@@ -1,1 +1,27 @@
 
+import java.util.Scanner;
+
+/**
+ * Exercício: Entrada de dados em Java
+ * Conteúdo: Scanner
+ * Objetivo: ler dados digitados pelo usuário
+ */
+
+public class ExemplosScanner {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("\nOlá, " + nome + "!");
+        System.out.println("Idade informada: " + idade + " anos.");
+
+        scanner.close();
+    }
+}
