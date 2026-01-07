197f565e6dac38c3a6a01cde45b0d33bec63b1fc
Natália Baptista Pastre <natalia.pastre10@gmail.com>
2026-01-07 20:40:49 -0300

    Create ProgramaPrincipal.java

---
+++ 05-poo-classe-pessoa/ProgramaPrincipal.java
@@ -0,0 +1,12 @@
+public class ProgramaPrincipal {
+
+    public static void main(String[] args) {
+
+        // Criar uma pessoa -> instanciar
+        //tipo(classe) var + new classe()
+        Pessoa pessoa1 = new Pessoa();
+
+        pessoa1.setNome("Natalia");
+        System.out.println(pessoa1.getNome());
+    }
+}
