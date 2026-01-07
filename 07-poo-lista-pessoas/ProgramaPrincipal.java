915df7502341d955c209a1e21733d905337d83e7
Natália Baptista Pastre <natalia.pastre10@gmail.com>
2026-01-07 20:57:09 -0300

    Create ProgramaPrincipal.java

---
+++ 07-poo-lista-pessoas/ProgramaPrincipal.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ProgramaPrincipal {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Pessoa> pessoas = new ArrayList<>();
+
+        int opcao;
+
+        do {
+            System.out.println("\n=== MENU ===");
+            System.out.println("1 - Cadastrar pessoa");
+            System.out.println("2 - Listar pessoas");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine(); // limpar buffer
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o nome: ");
+                    String nome = scanner.nextLine();
+
+                    System.out.print("Digite a idade: ");
+                    int idade = scanner.nextInt();
+
+                    pessoas.add(new Pessoa(nome, idade));
+                    System.out.println("Pessoa cadastrada com sucesso!");
+                    break;
+
+                case 2:
+                    System.out.println("\n--- Pessoas cadastradas ---");
+                    for (Pessoa p : pessoas) {
+                        System.out.println("Nome: " + p.getNome() + " | Idade: " + p.getIdade());
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Encerrando o programa...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+            }
+
+        } while (opcao != 0);
+
+        scanner.close();
+    }
+}
